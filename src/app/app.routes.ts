c442ad86f12ba870efb02c22e2989ae019af606a
=cdhai16 <chuduchai1901@gmail.com>
2026-07-03 09:49:57 +0700

    feat: cau hinh app routing (review, flashcard, import)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ReviewDashboardComponent } from './features/review-dashboard/review-dashboard.component';
+import { FlashcardComponent } from './features/flashcard/flashcard.component';
+import { VocabImportComponent } from './features/vocab-import/vocab-import.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ReviewDashboardComponent,
+  },
+  {
+    path: 'flashcard',
+    component: FlashcardComponent,
+  },
+  {
+    path: 'import',
+    component: VocabImportComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
